0cad85e46fe828df813e4df3af11612274bd2cba
trinhtn-1878 <nguyen.the.trinh@sun-asterisk.com>
2020-09-08 17:10:54 +0700

    create UI

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_createui/profileView.dart';
+
+main() async {
+  return runApp(
+    MaterialApp(
+      home: profileView(),
+    )
+  );
+}
